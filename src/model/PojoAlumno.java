8f64d1f4f3dc659ba7e670a4dcd5b23d808cd4a2
Navarro2111 <manpastor2@hotmail.com>
2018-04-24 13:26:29 +0200

    En este commit se añade el pojo alumno

---
+++ src/model/PojoAlumno.java
@@ -0,0 +1,29 @@
+package model;
+
+public class PojoAlumno {
+
+	private int nExp;//es el numero de expediente
+	private String nombre;//es el nombre del alumno
+	private String apellido1;//es el primer apellido
+	private String apellido2;//es el segundo apellido
+	public PojoAlumno(int nExp, String nombre, String apellido1, String apellido2) {
+		super();
+		this.nExp = nExp;
+		this.nombre = nombre;
+		this.apellido1 = apellido1;
+		this.apellido2 = apellido2;
+	}
+	public int getnExp() {
+		return nExp;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public String getApellido1() {
+		return apellido1;
+	}
+	public String getApellido2() {
+		return apellido2;
+	}
+	
+}
